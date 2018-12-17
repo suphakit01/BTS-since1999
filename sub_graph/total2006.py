65e1a76921a4bbe4f8c5274d0da37404d16ff9d7
Thivawat <42943141+Thivawat@users.noreply.github.com>
2018-12-17 14:27:21 +0700

    Add files via upload

---
+++ sub_graph/total2006.py
@@ -0,0 +1,28 @@
+""" Graph Total Ridership of BTS 2006 """
+
+import pygal
+def graph_2006():
+    """ plot graph with pygal """
+    date = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    y_label = [0, 5000000, 10000000, 15000000, 20000000]
+
+    line_chart = pygal.Line()
+    line_chart.title = 'Total Ridership of BTS 2006'
+
+    line_chart.x_labels = map(str, date)
+    line_chart.y_labels = map(int, y_label)
+    line_chart.add('Total Ridership', [11532519,
+        10665446,
+        12628541,
+        10133987,
+        11300664,
+        12067062,
+        12312414,
+        12617203,
+        11410102,
+        11752087,
+        12004291,
+        11624533])
+    line_chart.render_to_file("D:/PSIT/WEB/sub_graph/total_ridership2006.svg")
+
+graph_2006()
---
+++ sub_graph/total2007.py
@@ -0,0 +1,28 @@
+""" Graph Total Ridership of BTS 2007 """
+
+import pygal
+def graph_2007():
+    """ plot graph with pygal """
+    date = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    y_label = [0, 5000000, 10000000, 15000000, 20000000]
+
+    line_chart = pygal.Line()
+    line_chart.title = 'Total Ridership of BTS 2007'
+
+    line_chart.x_labels = map(str, date)
+    line_chart.y_labels = map(int, y_label)
+    line_chart.add('Total Ridership', [11175939,
+        10659843,
+        11496036,
+        9622967,
+        10660186,
+        11138186,
+        11184445,
+        11391791,
+        10854799,
+        11325870,
+        11663120,
+        10897320])
+    line_chart.render_to_file("D:/PSIT/WEB/sub_graph/total_ridership2007.svg")
+
+graph_2007()
