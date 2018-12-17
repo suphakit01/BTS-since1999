f0a30e7a09ae46a4e9e79b0e6889d7331aa5fd20
suphakit01 <43027793+suphakit01@users.noreply.github.com>
2018-12-17 14:01:43 +0700

    Add files via upload

---
+++ sub_graph/graph_1999.py
@@ -0,0 +1,28 @@
+""" Graph Total Ridership of BTS 1999 """
+
+import pygal
+def graph_1999():
+    """ plot graph with pygal """
+    date = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    y_label = [0, 5000000, 10000000, 15000000, 20000000]
+
+    line_chart = pygal.Line()
+    line_chart.title = 'Total Ridership of BTS 1999'
+
+    line_chart.x_labels = map(str, date)
+    line_chart.y_labels = map(int, y_label)
+    line_chart.add('Total Ridership', [None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        4585743])
+    line_chart.render_to_file("D:/PSIT/WEB/sub_graph/total_ridership1999.svg")
+
+graph_1999()
--- sub_graph/testme_new.py
+++ sub_graph/testme_new.py
@@ -4,19 +4,23 @@ import pandas
 def graph():
 
     #import src
-    data = pandas.read_excel("D:/PSIT/WEB/graph/srcdataridrship.xls")
+    data = pandas.read_excel("D:/PSIT/WEB/sub_graph/srcdataridrship_new.xls")
 
     #append to list
     passenger = []
+    date = []
+
     for i in data["Total Ridership1"]:
         if not str(i).isalpha():
             passenger.append(i)
+    for j in data["Date"]:
+        date.append(str(j))
 
     #print graph
     line_chart = pygal.Line()
     line_chart.title = 'Total Ridership of BTS (since 1999)'
 
-    line_chart.x_labels = map(str, range(1999, 2018))
+    line_chart.x_labels = map(str, date)
     line_chart.add('Total Ridership', passenger)
-    line_chart.render_to_file("D:/PSIT/WEB/graph/total_ridership.svg")
+    line_chart.render_to_file("D:/PSIT/WEB/sub_graph/total_ridership.svg")
 graph()
