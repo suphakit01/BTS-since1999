9aa2268ecd371c0272b4164f4cd83ec06c46f6e0
suphakit01 <43027793+suphakit01@users.noreply.github.com>
2018-12-17 16:18:44 +0700

    Add files via upload

--- sub_graph/graph_1999.py
+++ sub_graph/graph_1999.py
@@ -6,8 +6,7 @@ def graph_1999():
     date = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     y_label = [0, 5000000, 10000000, 15000000, 20000000]
 
-    line_chart = pygal.Line()
-    line_chart.title = 'Total Ridership of BTS 1999'
+    line_chart = pygal.Line(legend_at_bottom=True)
 
     line_chart.x_labels = map(str, date)
     line_chart.y_labels = map(int, y_label)
@@ -23,6 +22,7 @@ def graph_1999():
         None,
         None,
         4585743])
+    line_chart.title = 'Total Ridership of BTS 1999'
     line_chart.render_to_file("D:/PSIT/WEB/sub_graph/total_ridership1999.svg")
 
 graph_1999()
